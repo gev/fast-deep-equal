468f2117c83ddc711575220e609ae8c18f48cb7a
Evgeny <gazdovsky@gmail.com>
2018-03-16 11:42:04 +0300

    up

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-'use strict';
 
 var isArray = Array.isArray;
 var keyList = Object.keys;
-var hasProp = Object.prototype.hasOwnProperty;
 
-module.exports = function equal(a, b) {
+function equal(a, b) {
   if (a === b) return true;
 
   var arrA = isArray(a)
@@ -23,25 +21,12 @@ module.exports = function equal(a, b) {
 
   if (arrA != arrB) return false;
 
-  var dateA = a instanceof Date
-    , dateB = b instanceof Date;
-  if (dateA != dateB) return false;
-  if (dateA && dateB) return a.getTime() == b.getTime();
-
-  var regexpA = a instanceof RegExp
-    , regexpB = b instanceof RegExp;
-  if (regexpA != regexpB) return false;
-  if (regexpA && regexpB) return a.toString() == b.toString();
-
   if (a instanceof Object && b instanceof Object) {
     var keys = keyList(a);
     length = keys.length;
 
     if (length !== keyList(b).length)
       return false;
-
-    for (i = 0; i < length; i++)
-      if (!hasProp.call(b, keys[i])) return false;
 
     for (i = 0; i < length; i++) {
       key = keys[i];
@@ -52,4 +37,18 @@ module.exports = function equal(a, b) {
   }
 
   return false;
-};
+}
+
+function contains(a, b) {
+  var keys = keyList(b)
+    , key;
+
+  for (i = 0; i < keys.length; i++) {
+    key = keys[i];
+    if (!equal(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+module.exports = { equal, contains };
